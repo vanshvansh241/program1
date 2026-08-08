78ac6f399d661bc5f4e0a9923c8626d358ea9213
Vansh Pannu <vanshvansh241@gmail.com>
2026-08-08 16:08:10 +0530

    Add files via upload

---
+++ matplt.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+
+
+# x = [1,2,3,4,5]
+# y = [10,20,30,40,50]
+# plt.plot(x,y)
+# plt.title("simple line graph")
+# plt.xlabel("x axis")
+# plt.ylabel("y axis")
+# plt.show()
+
+# categories = ["general","SC","OBC"]
+# seats = [10,20,30]
+# plt.bar(categories,seats)
+# plt.title("total seats by categories")
+# plt.xlabel("categories")
+# plt.ylabel("seats")
+# plt.show()
+
+# categories = ["general","SC","OBC"]
+# seats = [10,20,30]
+# plt.barh(categories,seats)
+# plt.title("total seats by categories")
+# plt.xlabel("categories")
+# plt.ylabel("seats")
+# plt.show()
+
+
+# values = [20,25,30,25]
+# labels = ["A","B","C","D"]
+# plt.pie(values,labels=labels,autopct="%1.1f%%")
+# plt.title("pie chart")
+# plt.show()
+
+x = [1,2,3,4,5]
+y = [10,20,30,40,50]
+plt.plot(x,y)
+plt.title("simple line graph")
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.grid(True)
+plt.show()
